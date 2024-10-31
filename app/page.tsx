ef6107674d900043533a43b20af2fb2b19d7e75e
Jacob Cell <59305245+Thirstymidget28@users.noreply.github.com>
2024-10-31 17:21:16 -0500

    Updated Home styling.
    Fixed top margin for Navbar, to prevent overlapping.

--- app/page.tsx
+++ app/page.tsx
@@ -12,40 +12,42 @@ const montserrat = Montserrat({
 // Home Page
 export default function Home() {
   return (
-    <main className="bg-hero bg-cover bg-center min-h-screen w p-2 pt-28 flex flex-col content-center items-center ">
-      {/* <h1>Hello World</h1> */}
-      {/* <Navbar /> */}
+    <main className="h-screen pt-20">
+      <div className="bg-hero bg-cover bg-center min-h-screen w p-2 flex flex-col content-center items-center">
+        {/* <h1>Hello World</h1> */}
+        {/* <Navbar /> */}
 
-      {/* container for texas tech banner */}
-      <div className="w-full mt-24 pb-5 sm:w-4/5 lg:w-3/5">
-        <Image
-          src="/TTU_Word_Mark.png"
-          alt="TTU DoubleT"
-          width={1000}
-          height={1000}
-          style={{ width: "100%", height: "100%" }}
-        />
+        {/* container for texas tech banner */}
+        <div className="w-full mt-24 pb-5 sm:w-4/5 lg:w-3/5">
+          <Image
+            src="/TTU_Word_Mark.png"
+            alt="TTU DoubleT"
+            width={1000}
+            height={1000}
+            style={{ width: "100%", height: "100%" }}
+          />
+        </div>
+        <h1
+          className={`${montserrat.className} text-4xl text-red-600 font-bold pb-0 sm:text-5xl md:text-6xl 2xl:text-8xl`}
+        >
+          Red Rater
+        </h1>
+        <p
+          className={`${montserrat.className} text-sm font-bold text-center pb-5 sm:text-base lg:text-lg 2xl:text-2xl`}
+        >
+          Professor and Course Evaluation Tool
+        </p>
+        <div className="pt-10 w-auto sm:w-1/2 lg:w-2/5 2xl:w-1/4">
+          <SearchBar />
+        </div>
+        <label
+          htmlFor="ai-drawer"
+          className={`${montserrat.className} btn shadow-none border-none bg-transparent hover:bg-transparent text-xs font-bold text-center pb-5 sm:text-sm lg:text-md 2xl:text-lg px-10
+            `}
+        >
+          Can't find the right fit? Click here to try our AI tool.
+        </label>
       </div>
-      <h1
-        className={`${montserrat.className} text-4xl text-red-600 font-bold pb-0 sm:text-5xl md:text-6xl 2xl:text-8xl`}
-      >
-        Red Rater
-      </h1>
-      <p
-        className={`${montserrat.className} text-sm font-bold text-center pb-5 sm:text-base lg:text-lg 2xl:text-2xl`}
-      >
-        Professor and Course Evaluation Tool
-      </p>
-      <div className="pt-10 w-auto sm:w-1/2 lg:w-2/5 2xl:w-1/4">
-        <SearchBar />
-      </div>
-      <label
-        htmlFor="ai-drawer"
-        className={`${montserrat.className} btn shadow-none border-none bg-transparent hover:bg-transparent text-xs font-bold text-center pb-5 sm:text-sm lg:text-md 2xl:text-lg px-10
-          `}
-      >
-        Can't find the right fit? Click here to try our AI tool.
-      </label>
     </main>
   );
 }
