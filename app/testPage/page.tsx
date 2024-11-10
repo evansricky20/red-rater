da8dc73a5b2183e6280b4987a64de860e13b6ead
ricky <rickyevans@hotmail.com>
2024-11-10 14:07:14 -0600

    page professor name fix

--- app/testPage/page.tsx
+++ app/testPage/page.tsx
@@ -1,32 +1,32 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 interface Professor {
-  name: string;
+  Name: string;
 }
 
 export default function Dashboard() {
   const [professors, setProfessors] = useState<Professor[]>([]);
 
   useEffect(() => {
-  console.log("Fetching data from /api/professors...");
-  async function fetchData() {
-    const response = await fetch('/api/professors');
-    const data = await response.json();
-    console.log("Data fetched:", data); // Log fetched data
-    setProfessors(data);
-  }
+    console.log("Fetching data from /api/professors...");
+    async function fetchData() {
+      const response = await fetch("/api/professors");
+      const data = await response.json();
+      console.log("Data fetched:", data); // Log fetched data
+      setProfessors(data);
+    }
 
-  fetchData().catch(console.error);
-}, []);
+    fetchData().catch(console.error);
+  }, []);
 
   return (
     <div>
       <h1>Dashboard</h1>
       <ul>
         {professors.map((professor, index) => (
-          <li key={index}>{professor.name}</li> // Render name only
+          <li key={index}>{professor.Name}</li> // Render name only
         ))}
       </ul>
     </div>
