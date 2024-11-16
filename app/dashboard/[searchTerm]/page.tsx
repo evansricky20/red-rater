8fa1e79b54fed133e0188879b8934638b946e958
ricky <rickyevans@hotmail.com>
2024-11-16 12:17:08 -0600

    Added overall rating and course rating to dashboard

--- app/dashboard/[searchTerm]/page.tsx
+++ app/dashboard/[searchTerm]/page.tsx
@@ -1,7 +1,7 @@
-import { notFound } from 'next/navigation';
-import InfoCard from './../../components/InfoCard';
-import Image from 'next/image';
-import TestimonialCard from './../../components/TestimonialCard';
+import { notFound } from "next/navigation";
+import InfoCard from "./../../components/InfoCard";
+import Image from "next/image";
+import TestimonialCard from "./../../components/TestimonialCard";
 
 interface DashboardProps {
   params: { searchTerm: string };
@@ -13,14 +13,18 @@ const Dashboard = async ({ params, searchParams }: DashboardProps) => {
   const { baseURL } = await searchParams;
 
   // Fetch HTTP response from '/api/professors'
-  const response = await fetch(`${baseURL}/api/professors/${encodeURIComponent(searchTerm)}`);
-  
-  if (!response.ok) { // Confirm successful HTTP response
+  const response = await fetch(
+    `${baseURL}/api/professors/${encodeURIComponent(searchTerm)}`
+  );
+
+  if (!response.ok) {
+    // Confirm successful HTTP response
     notFound();
   }
   const data = await response.json();
 
-  if (!data || data.length === 0) { // Confirm data was retrieved from DB
+  if (!data || data.length === 0) {
+    // Confirm data was retrieved from DB
     notFound();
   }
 
@@ -28,30 +32,30 @@ const Dashboard = async ({ params, searchParams }: DashboardProps) => {
     <main className="h-screen">
       <div className="bg-hero bg-cover bg-center pt-20">
         <section className="info-section bg-white h-fit w-10/12 flex justify-center mx-auto mt-10">
-          <InfoCard searchTerm={searchTerm}/>
+          <InfoCard searchTerm={searchTerm} />
         </section>
       </div>
       <section className="testimonial-section h-fit flex flex-col justify-center mx-auto">
-        <div className="flex-1 border-b-4 border-black bg-red-600 font-bold">
+        <div className="flex-1 content-center border-b-4 border-black bg-red-600 font-bold">
           <div className="lineart-element flex">
-            <div style={{ transform: 'scaleX(-1)' }}>
-              <Image
-                src="/TTU_Horizontal_Lineart_black.png"
-                alt="TTU line art decal."
-                width={1000}
-                height={100}
-                style={{ width: '100%', height: '100%' }}
-              />
-            </div>
+            <Image
+              src="/TTU_Horizontal_Lineart_black.png"
+              alt="TTU line art decal."
+              width={1000}
+              height={100}
+              style={{ width: "50%", height: "100%" }}
+            />
             <Image
               src="/TTU_Horizontal_Lineart_white.png"
               alt="TTU line art decal."
               width={1000}
               height={100}
-              style={{ width: '50%', height: '100%' }}
+              style={{ width: "50%", height: "100%" }}
             />
           </div>
-          <h2 className="text-center text-3xl text-white p-4">Student Testimonials</h2>
+          <h2 className="text-center text-3xl text-white p-4">
+            Student Testimonials
+          </h2>
         </div>
 
         <div>
@@ -59,11 +63,21 @@ const Dashboard = async ({ params, searchParams }: DashboardProps) => {
             <div className="testimonial-buttons w-7/12 ml-auto flex justify-between py-5 px-6">
               <button className="btn btn-outline place-self-center border-2">
                 New Post
-                <Image src="/plus.svg" alt="Plus symbol" width={20} height={20} />
+                <Image
+                  src="/plus.svg"
+                  alt="Plus symbol"
+                  width={20}
+                  height={20}
+                />
               </button>
               <button className="btn btn-outline border-2">
                 Filter
-                <Image src="/filter.svg" alt="Filter symbol" width={20} height={20} />
+                <Image
+                  src="/filter.svg"
+                  alt="Filter symbol"
+                  width={20}
+                  height={20}
+                />
               </button>
             </div>
           </div>
